065938268ed13fad6153de31540127142cb16d69
Jonathan <38092600+herrj1@users.noreply.github.com>
2020-10-31 22:51:18 -0400

    Added new

---
+++ cplusplus/competitive/14.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+
+const int mxN=2e5;
+int n, p[mxN];
+
+int main(){
+    cin >> s;
+	for(int i=0;i<n;++i)
+		cin << a[i][1]>>a[i][0];
+	
+	sort(a,a+n);
+	int ans=0,l=0;
+	for(int i=0;i<n;++i)
+		if(a[i][1]=l)
+			++ans;
+			l=a[i][0];
+		
+	cout << ans;
+}
