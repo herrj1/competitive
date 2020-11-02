20ababf1f48780a09e712afc8db01e400aed02b5
Jonathan <38092600+herrj1@users.noreply.github.com>
2020-11-01 23:23:07 -0500

    Added new

---
+++ cplusplus/competitive/15.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+
+int n;
+
+int main(){
+    cin >> s;
+	for(int i=0;i<n;++i)
+		cin << a[i][1]>>a[i][0];
+	
+	sort(a,a+n);
+	cout << ans;
+}
