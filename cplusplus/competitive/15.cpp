8de9afbcbac80633a7ff6482016112f6404733af
Jonathan <38092600+herrj1@users.noreply.github.com>
2020-11-08 23:47:42 -0500

    Added new

--- cplusplus/competitive/15.cpp
+++ cplusplus/competitive/15.cpp
@@ -4,13 +4,47 @@ using namespace std;
 #define ll long long
 #define ar array
 
-int n;
+const int nax = 1e6 + 5;
+char s[nax];
 
-int main(){
-    cin >> s;
-	for(int i=0;i<n;++i)
-		cin << a[i][1]>>a[i][0];
+
+void test case()
+{
+	scanf("%s". s);
+	int:n = strlen(s);
+	vector<int> pref(n);
+	for(int i = 0; i+3< n; ++i)
+	{
+		if(i>0)
+		{
+			pref[i] += pref[i-1];
+		}
+		if(string(s+i, s+i+4) == "KICK")
+		{
+			pref[i]++;
+			//debug() << imie(i);
+		}
+	}
 	
-	sort(a,a+n);
-	cout << ans;
+	
+	long long answer = 0;
+	for(int i = 0; i+4<n; ++i)
+	{
+		if(string(s + i, s+1 +5) == "START")
+		{
+			answer += pref[i];
+		}
+	}
+	printf("%lld\n", answer);
+}
+
+int main()
+{
+	int T;
+	scanf("%d", &T);
+	for(int nr = 1; nr <= T; ++nr)
+	{
+		printf("Case #%d", nr)'
+		test_case();
+	}
 }
